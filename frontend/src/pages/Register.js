d6bef744d799934543ebd19c9ec92d35fa2d92a1
shubhamsawant18 <shubhsawant124@gmail.com>
2025-06-05 18:50:40 +0530

    added .env frontend

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -14,8 +14,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-  const response = await fetch("http://localhost:5000/api/auth/register", {
-  method: "POST",
+const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/register`, {   method: "POST",
   headers: {
     "Content-Type": "application/json",
   },
