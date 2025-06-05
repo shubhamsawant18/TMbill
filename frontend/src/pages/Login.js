f205b493a9f8478722d7dc6aef77baa495246f50
shubhamsawant18 <shubhsawant124@gmail.com>
2025-06-05 18:58:50 +0530

    deployment changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {  
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(loginData),
